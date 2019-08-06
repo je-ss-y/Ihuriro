60891bc2138a67a89b10dc3b335bfad51591c1b7
je-ss-y <nkujess@gmail.com>
2019-08-06 11:21:26 +0200

    first commit.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD6Dkj_XwjktukHoJNj9U7EJ_9W6RQsGeo",
+    authDomain: "ihuriro-a79d9.firebaseapp.com",
+    databaseURL: "https://ihuriro-a79d9.firebaseio.com",
+    projectId: "ihuriro-a79d9",
+    storageBucket: "",
+    messagingSenderId: "1046180308670",
+    appId: "1:1046180308670:web:30468d53728afead"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
