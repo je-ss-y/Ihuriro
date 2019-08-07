3bc8e208765b9b6b633705b738265e33fb22314e
je-ss-y <nkujess@gmail.com>
2019-08-07 15:48:38 +0200

    add morechanges to fire base  and text area element.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyD6Dkj_XwjktukHoJNj9U7EJ_9W6RQsGeo",
-    authDomain: "ihuriro-a79d9.firebaseapp.com",
-    databaseURL: "https://ihuriro-a79d9.firebaseio.com",
-    projectId: "ihuriro-a79d9",
-    storageBucket: "",
-    messagingSenderId: "1046180308670",
-    appId: "1:1046180308670:web:30468d53728afead"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAhgvRKrcHIlfmkTtojizNW4GVT5u_A4oU",
+  authDomain: "ihuri2.firebaseapp.com",
+  databaseURL: "https://ihuri2.firebaseio.com",
+  projectId: "ihuri2",
+  storageBucket: "",
+  messagingSenderId: "101867079535",
+  appId: "1:101867079535:web:b3d7fc1fd9257917"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
   function signUp(){
     var email = document.getElementById("textEmail").value;
     var password = document.getElementById("textPassword").value;
@@ -22,6 +23,27 @@ var firebaseConfig = {
   //   var lastName = document.getElementById("textlastname").value;
 
   // })
- function myFunction(){
-   location.replace("directIt");
- }
+  function postMyComment(){
+    
+    // var usersRef = ref.child("comment");
+    var holdText = document.getElementById("myComment").value;
+    var obj = {userText: holdText};
+    // usersRef.set(obj);
+    firebase.database().ref('/Comments').push(obj).then(function(){alert("success")}).catch(function(error) {
+      console.log(error.code);
+     });
+    firebase.database().ref("UserData").orderByKey();
+    var userDataRef = firebase.database().ref("UserData").orderByKey();
+userDataRef.once("value")
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var key = childSnapshot.key;
+      var childData = childSnapshot.val();              // childData will be the actual contents of the child
+
+      var name_val = childSnapshot.val().Name;
+      var id_val = childSnapshot.val().AssignedID;
+      document.getElementById("name").innerHTML = name_val;
+      document.getElementById("id").innerHTML = id_val;
+  });
+ });
+}
